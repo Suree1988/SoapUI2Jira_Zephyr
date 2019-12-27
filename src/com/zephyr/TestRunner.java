42adff708b6cd09cfb430bf850d02738f21a978e
Suree1988 <44668926+Suree1988@users.noreply.github.com>
2019-12-27 17:39:18 +0100

    Update TestRunner.java

--- src/com/zephyr/TestRunner.java
+++ src/com/zephyr/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;		
 
 @RunWith(Cucumber.class)				
-@CucumberOptions(features= {"bdd-export5718803273015028755"},
+@CucumberOptions(features= {"bdd-export571880327301502875"},
 glue= {"com.zephyr"},
 format= {"junit:target/junit/Test-soaui_report.xml"})		
 
