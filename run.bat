cb6ddb9904dfdf70efa8d2657dea71f7809a8ffc
Suree1988 <44668926+Suree1988@users.noreply.github.com>
2019-12-27 17:22:39 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,2 @@
 javac -cp ./lib/*  src/com/zephyr/TestRunner.java src/com/zephyr/myFirstSoapUITest.java
-java -cp ./bin;./lib/* org.junit.runner.JUnitCore com.zephyr.TestRunner
+java -cp ./src;./lib/* org.junit.runner.JUnitCore com.zephyr.TestRunner
